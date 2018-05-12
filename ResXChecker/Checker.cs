51b620c972318034ca4c0a75567ec59d67f6efa6
Florian Mücke <fmuecke@gmail.com>
2018-05-12 23:28:24 +0200

    initial commit

---
+++ ResXChecker/Checker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResXChecker
+{
+    class Checker
+    {
+        internal static void PrintUnreferenced(string resx, string filePattern, string baseDir)
+        {
+            if (string.IsNullOrWhiteSpace(filePattern))
+            {
+                filePattern = "*.cs;*.xaml";
+            }
+
+            if (string.IsNullOrWhiteSpace(baseDir))
+            {
+                baseDir = System.IO.Directory.GetCurrentDirectory();
+            }
+
+            var entries = fmdev.ResX.ResXFile.Read(resx);
+            entries = entries.OrderBy(e => e.Id).ToList();
+
+            var files = System.IO.Directory.EnumerateFiles(baseDir, filePattern, System.IO.SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var content = System.IO.File.ReadAllText(file);
+                foreach (var entry in entries.ToList())
+                {
+                    if (content.Contains(entry.Id))
+                    {
+                        entries.Remove(entry);
+                    }
+                }
+            }
+
+            Console.WriteLine($"{entries.Count} Unreferenced entries");
+            entries.ForEach(e => Console.WriteLine(e.Id));
+        }
+    }
+}
--- ResXChecker/Program.cs
+++ ResXChecker/Program.cs
@@ -8,8 +8,29 @@ namespace ResXChecker
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var parser = new fmdev.ArgsParser.ArgsParser(typeof(CommandArgs));
+            if (parser.Parse(args))
+            {
+                if (parser.Result is CommandArgs.UnrefCommand)
+                {
+                    PrintUnreferenced(parser.Result as CommandArgs.UnrefCommand);
+                }
+                else
+                {
+                    parser.PrintUsage();
+                }
+            }
+
+            return 0;
+        }
+
+        private static int PrintUnreferenced(CommandArgs.UnrefCommand cmd)
+        {
+            Checker.PrintUnreferenced(cmd.Resx, cmd.FileTypes, cmd.BaseDir);
+
+            return 0;
         }
     }
 }
